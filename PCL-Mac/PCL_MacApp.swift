351171fae460c80aa3b9d1f2719b636855c4586f
YiZhiMCQiu <pikachu20130124@163.com>
2025-05-17 14:03:55 +0800

    Initial Commit

---
+++ PCL-Mac/PCL_MacApp.swift
@@ -0,0 +1,17 @@
+//
+//  PCL_MacApp.swift
+//  PCL-Mac
+//
+//  Created by YiZhiMCQiu on 2025/5/17.
+//
+
+import SwiftUI
+
+@main
+struct PCL_MacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
